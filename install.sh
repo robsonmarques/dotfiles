00c50051fe85c460e0fac89952127a2d0b82c90b
Diego Schell Fernandes <diego.schell.f@gmail.com>
2015-12-14 19:30:13 -0200

    Added vim-plug as dependency to install

--- install.sh
+++ install.sh
@@ -18,5 +18,9 @@ git clone https://github.com/magicmonty/bash-git-prompt.git $HOME/bash-git-promp
 
 source $HOME/.profile
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 echo "Installing vim plugins"
 vim +PlugInstall +qall
